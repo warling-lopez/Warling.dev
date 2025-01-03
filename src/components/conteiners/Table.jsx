55f792fd541d7061594261818de338e5fe97b97c
WarHub <warlinglopez01@gmail.com>
2025-01-02 22:15:49 -0400

    camion

--- src/components/conteiners/Table.jsx
+++ src/components/conteiners/Table.jsx
@@ -12,7 +12,7 @@ function Table() {
       <Skills icon={cloud} title={'Cloud Computing'} desc={'Uso de recursos informáticos a través de Internet, como almacenamiento y procesamiento de datos en la nube.'}/>
       <Skills icon={plane} title={'Desarrollo veloz'} desc={'Creamos soluciones ágiles y eficientes, adaptándonos rápidamente a las necesidades de tu proyecto para que alcances tus objetivos en tiempo récord.'}/>
       <Skills icon={seo} title={'Seo'} desc={'Mejorar la visibilidad de un sitio web en los resultados de búsqueda para atraer más tráfico orgánico.'}/>
-      <Skills icon={stadistica} title={'Seo-camp'} desc={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ducimus inventore, quam maxime tenetur.'}/>
+      <Skills icon={stadistica} title={'gana mas dinero'}desc={'Optimiza tus recursos y maximiza tus ingresos con estrategias diseñadas para hacer crecer tu negocio de manera sostenible.'}/>
       <Skills icon={color} title={'Gestión de proyectos'} desc={'Capacidad para planificar, coordinar y ejecutar proyectos de principio a fin, asegurando que se cumplan los objetivos.'}/>
       <Skills icon={zap} title={'Automatización '} desc={'Optimización del rendimiento del código para que las aplicaciones funcionen de manera más rápida y eficiente.'}/>
       
