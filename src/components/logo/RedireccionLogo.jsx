57e53f08b55f5154b991fb945335671df0a8b64c
warling <warlinglopez01@gmail.com>
2025-07-14 02:16:55 -0400

    remplase logo redirecion

--- src/components/logo/RedireccionLogo.jsx
+++ src/components/logo/RedireccionLogo.jsx
@@ -1,17 +1,14 @@
 "use client";
-import { Allan } from "next/font/google";
-import React, { useEffect, useState } from "react";
 
 export default function Logo() {
   
   const outerStyle = {
-    position: initConditional ? "absolute" : "relative",
-    top: initConditional ? "40vh" : "10vh",
-    left: expandirDiamante ? "-400px" : 0,
+    position: "absolute",
+    top: "10vh",
+    left: "-400px",
     width: "100vw",
-    height: initConditional ? undefined : "auto",
-    display: initConditional ? "grid" : "flex",
-    transition: initConditional ? "top 1s ease-out" : undefined,
+    height: "auto",
+    display: "grid",
     justifyContent: "center",
     alignItems: "center",
     transition: "all 1s ease-in-out", // Animación suave
@@ -31,8 +28,8 @@ export default function Logo() {
   const diamondStyle = {
     position: "absolute",
     top: 0,
-    width: expandirDiamante ? "300px" : "50px",
-    height: expandirDiamante ? "600px" : "100px",
+    width: "300",
+    height: "600",
     background: "radial-gradient(var(--specialColor), var(--specialColorLogo))",
     clipPath: "polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%)",
     boxShadow: "1px 6px 12px #333",
